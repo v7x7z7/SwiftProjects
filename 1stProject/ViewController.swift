b7e0c718cf59400bcd3215a6b632ab8293e5727e
Aero <aero@gmail.com>
2024-08-07 15:59:53 +0500

    Initial Commit

---
+++ 1stProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1stProject
+//
+//  Created by vv 77 on 07/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
